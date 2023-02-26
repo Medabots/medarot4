c6e5ea3b1f5115b0e92b1c4f3330a158db2cb684
andwhyisit <realmmaster26@hotmail.com>
2023-02-26 21:10:42 +1000

    More disasms.

---
+++ game/src/text/main_script.asm
@@ -0,0 +1,158 @@
+INCLUDE "game/src/common/constants.asm"
+
+SECTION "Main Script Variables 1", WRAM0[$C4C3]
+W_MainScriptPointerLocationOffset:: ds 2
+W_MainScriptPauseTimer:: ds 1
+W_MainScriptLineMappingBaseLocation:: ds 2
+W_MainScriptSpeed:: ds 1
+W_MainScriptIterator:: ds 1
+W_MainScriptKanjiDrawingRegionTileIndex:: ds 1
+W_MainScriptPortraitCharacter:: ds 1
+W_MainScriptTextBank:: ds 1
+W_MainScriptExitMode:: ds 1
+
+SECTION "Main Script Variables 2", WRAM0[$C4D6]
+W_MainScriptCCSubState:: ds 1
+W_MainScriptPauseAutoAdvanceTimer:: ds 1
+
+SECTION "Main Script Variables 3", WRAM0[$C539]
+W_MainScriptPortraitPriorPlacement:: ds 1
+W_MainScriptPortraitPlacement:: ds 1
+W_MainScriptPortraitExpression:: ds 1
+
+; W_MainScriptPortraitPlacement has 5 viable values, those being $00, $01, $10, $11, and $FF.
+; $00 and $10 have left-facing portraits, and $01 and $11 have right-facing portraits.
+; $00 and $01 are of the left side of the screen. $10 and $11 are to the right.
+; $FF clears the current portrait.
+
+SECTION "Main Script", ROM0[$1C19]
+InitiateMainScript::
+  xor a
+  ld [W_MainScriptKanjiDrawingRegionTileIndex], a
+  ld [W_MainScriptPointerLocationOffset], a
+  ld [W_MainScriptPointerLocationOffset + 1], a
+  ld [W_MainScriptIterator], a
+  ld [W_MainScriptExitMode], a
+  ld [W_MainScriptPauseTimer], a
+  ld [W_MainScriptCCSubState], a
+  ld [W_MainScriptPauseAutoAdvanceTimer], a
+  ld a, 0
+  ld [W_MainScriptSpeed], a
+  ld a, $FF
+  ld [W_MainScriptPortraitCharacter], a
+  ld [W_MainScriptPortraitPriorPlacement], a
+  ld [W_MainScriptPortraitPlacement], a
+  ld [W_MainScriptPortraitExpression], a
+  ld hl, $9C21
+  ld a, h
+  ld [W_MainScriptLineMappingBaseLocation], a
+  ld [$C9BE], a
+  ld a, l
+  ld [W_MainScriptLineMappingBaseLocation + 1], a
+  ld [$C9BF], a
+  ld a, 1
+  jp $13EE
+
+InitiateMainScriptAlternate::
+  xor a
+  ld [W_MainScriptKanjiDrawingRegionTileIndex], a
+  ld [W_MainScriptPointerLocationOffset], a
+  ld [W_MainScriptPointerLocationOffset + 1], a
+  ld [W_MainScriptIterator], a
+  ld [W_MainScriptExitMode], a
+  ld [W_MainScriptPauseTimer], a
+  ld [W_MainScriptCCSubState], a
+  ld [W_MainScriptPauseAutoAdvanceTimer], a
+  ld a, 0
+  ld [W_MainScriptSpeed], a
+  ld a, $FF
+  ld [W_MainScriptPortraitCharacter], a
+  ld [W_MainScriptPortraitPriorPlacement], a
+  ld [W_MainScriptPortraitPlacement], a
+  ld [W_MainScriptPortraitExpression], a
+  ld hl, $9C21
+  ld a, h
+  ld [W_MainScriptLineMappingBaseLocation], a
+  ld [$C9BE], a
+  ld a, l
+  ld [W_MainScriptLineMappingBaseLocation + 1], a
+  ld [$C9BF], a
+  ld bc, 0
+  ld e, 1
+  ld a, 0
+  jp DecompressTilemap1
+
+MapMainScriptWindow::
+  ld bc, 0
+  ld e, 1
+  ld a, 0
+  jp DecompressTilemap1
+
+SECTION "Main Script 2", ROM0[$1CC4]
+MainScriptProcessor::
+  call LoadTextPointer
+  ld a, [W_MainScriptExitMode]
+  or a
+  ret nz
+  ld a, [W_MainScriptPauseTimer]
+  or a
+  jr z, .doNotPause
+  dec a
+  ld [W_MainScriptPauseTimer], a
+  ld a, [$C522]
+  cp 2
+  ret z
+  ldh a, [H_JPInputChanged]
+  and M_JPInputA
+  jr nz, .aButtonPressed
+  ldh a, [H_JPInputHeldDown]
+  and M_JPInputA
+  ret z
+  ld a, [W_MainScriptPauseAutoAdvanceTimer]
+  cp $C
+  jr z, .aButtonPressed
+  inc a
+  ld [W_MainScriptPauseAutoAdvanceTimer], a
+  ret
+
+.aButtonPressed
+  xor a
+  ld [W_MainScriptPauseTimer], a
+  ld [W_MainScriptPauseAutoAdvanceTimer], a
+  ret
+
+.doNotPause
+  sla c
+  rl b
+  add hl, bc
+  ld a, [hli]
+  ld h, [hl]
+  ld l, a
+
+MainScriptProcessorPutCharLoop::
+  push hl
+  ld a, [W_MainScriptPointerLocationOffset]
+  ld b, a
+  ld a, [W_MainScriptPointerLocationOffset + 1]
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  cp $CC
+  jp z, $1D3E ; ControlCodeCC ; End code.
+  cp $CD
+  jp z, $1DF1 ; ControlCodeCD ; Newline code.
+  cp $CE
+  jp z, $1E1E ; ControlCodeCE ; Text speed code.
+  cp $CF
+  jp z, $1E47 ; ControlCodeCF ; New page after input code.
+  cp $D0
+  jp z, $1EAA ; ControlCodeD0 ; Print subtext code.
+  cp $D1
+  jp z, $1F50 ; ControlCodeD1 ; New page without input code.
+  cp $D2
+  jp z, $1F96 ; ControlCodeD2 ; Portrait display code.
+  cp $D3
+  jp z, $2142 ; ControlCodeD3 ; Kanji drawing code.
+  cp $D4
+  jp z, $2193 ; ControlCodeD4 ; Red text drawing code.
+  jp $1F6F ; MainScriptMapCharacter ; Map character to the screen.
