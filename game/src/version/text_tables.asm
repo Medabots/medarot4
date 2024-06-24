23022b233daa92b988b1d46f1b000e640f4baa12
VariantXYZ <VariantXYZ@users.noreply.github.com>
2024-06-23 17:44:06 -0700

    Note ends of text sections

--- game/src/version/text_tables.asm
+++ game/src/version/text_tables.asm
@@ -3,222 +3,277 @@ INCLUDE "./build/dialog/text_table_constants_{GAMEVERSION}.asm"
 SECTION "TextSection00", ROMX[$5504], BANK[$14]
 TextSection00:
   INCBIN cTextSection00
+TextSection00End:
 
 SECTION "TextSection01", ROMX[$4000], BANK[$46]
 TextSection01:
   INCBIN cTextSection01
+TextSection01End:
 
 SECTION "TextSection02", ROMX[$4000], BANK[$48]
 TextSection02:
   INCBIN cTextSection02
+TextSection02End:
 
 SECTION "TextSection03", ROMX[$5675], BANK[$48]
 TextSection03:
   INCBIN cTextSection03
+TextSection03End:
 
 SECTION "TextSection04", ROMX[$5c17], BANK[$48]
 TextSection04:
   INCBIN cTextSection04
+TextSection04End:
 
 SECTION "TextSection05", ROMX[$61ba], BANK[$48]
 TextSection05:
   INCBIN cTextSection05
+TextSection05End:
 
 SECTION "TextSection06", ROMX[$7042], BANK[$48]
 TextSection06:
   INCBIN cTextSection06
+TextSection06End:
 
 SECTION "TextSection07", ROMX[$4da3], BANK[$46]
 TextSection07:
   INCBIN cTextSection07
+TextSection07End:
 
 SECTION "TextSection08", ROMX[$4000], BANK[$47]
 TextSection08:
   INCBIN cTextSection08
+TextSection08End:
 
 SECTION "TextSection09", ROMX[$43c6], BANK[$32]
 TextSection09:
   INCBIN cTextSection09
+TextSection09End:
 
 SECTION "TextSection10", ROMX[$5ffc], BANK[$7b]
 TextSection10:
   INCBIN cTextSection10
+TextSection10End:
 
 SECTION "TextSection11", ROMX[$5592], BANK[$47]
 TextSection11:
   INCBIN cTextSection11
+TextSection11End:
 
 SECTION "TextSection12", ROMX[$5d38], BANK[$ff]
 TextSection12:
   INCBIN cTextSection12
+TextSection12End:
 
 SECTION "TextSection13", ROMX[$64c8], BANK[$f9]
 TextSection13:
   INCBIN cTextSection13
+TextSection13End:
 
 SECTION "TextSection14", ROMX[$5d58], BANK[$ff]
 TextSection14:
   INCBIN cTextSection14
+TextSection14End:
 
 SECTION "TextSection15", ROMX[$68a1], BANK[$f7]
 TextSection15:
   INCBIN cTextSection15
+TextSection15End:
 
 SECTION "TextSection16", ROMX[$4cf0], BANK[$58]
 TextSection16:
   INCBIN cTextSection16
+TextSection16End:
 
 SECTION "TextSection17", ROMX[$6898], BANK[$ff]
 TextSection17:
   INCBIN cTextSection17
+TextSection17End:
 
 SECTION "TextSection18", ROMX[$6745], BANK[$47]
 TextSection18:
   INCBIN cTextSection18
+TextSection18End:
 
 SECTION "TextSection19", ROMX[$4000], BANK[$98]
 TextSection19:
   INCBIN cTextSection19
+TextSection19End:
 
 SECTION "TextSection20", ROMX[$4000], BANK[$99]
 TextSection20:
   INCBIN cTextSection20
+TextSection20End:
 
 SECTION "TextSection21", ROMX[$4000], BANK[$9a]
 TextSection21:
   INCBIN cTextSection21
+TextSection21End:
 
 SECTION "TextSection22", ROMX[$4000], BANK[$9b]
 TextSection22:
   INCBIN cTextSection22
+TextSection22End:
 
 SECTION "TextSection23", ROMX[$4000], BANK[$9c]
 TextSection23:
   INCBIN cTextSection23
+TextSection23End:
 
 SECTION "TextSection24", ROMX[$4000], BANK[$9d]
 TextSection24:
   INCBIN cTextSection24
+TextSection24End:
 
 SECTION "TextSection25", ROMX[$4000], BANK[$bd]
 TextSection25:
   INCBIN cTextSection25
+TextSection25End:
 
 SECTION "TextSection26", ROMX[$4000], BANK[$ba]
 TextSection26:
   INCBIN cTextSection26
+TextSection26End:
 
 SECTION "TextSection27", ROMX[$4000], BANK[$bb]
 TextSection27:
   INCBIN cTextSection27
+TextSection27End:
 
 SECTION "TextSection28", ROMX[$4000], BANK[$bc]
 TextSection28:
   INCBIN cTextSection28
+TextSection28End:
 
 SECTION "TextSection29", ROMX[$4000], BANK[$9e]
 TextSection29:
   INCBIN cTextSection29
+TextSection29End:
 
 SECTION "TextSection30", ROMX[$4000], BANK[$9f]
 TextSection30:
   INCBIN cTextSection30
+TextSection30End:
 
 SECTION "TextSection31", ROMX[$4000], BANK[$a0]
 TextSection31:
   INCBIN cTextSection31
+TextSection31End:
 
 SECTION "TextSection32", ROMX[$4000], BANK[$a1]
 TextSection32:
   INCBIN cTextSection32
+TextSection32End:
 
 SECTION "TextSection33", ROMX[$4000], BANK[$e7]
 TextSection33:
   INCBIN cTextSection33
+TextSection33End:
 
 SECTION "TextSection34", ROMX[$4000], BANK[$20]
 TextSection34:
   INCBIN cTextSection34
+TextSection34End:
 
 SECTION "TextSection35", ROMX[$4000], BANK[$a2]
 TextSection35:
   INCBIN cTextSection35
+TextSection35End:
 
 SECTION "TextSection36", ROMX[$4000], BANK[$a3]
 TextSection36:
   INCBIN cTextSection36
+TextSection36End:
 
 SECTION "TextSection37", ROMX[$4000], BANK[$a4]
 TextSection37:
   INCBIN cTextSection37
+TextSection37End:
 
 SECTION "TextSection38", ROMX[$4000], BANK[$a5]
 TextSection38:
   INCBIN cTextSection38
+TextSection38End:
 
 SECTION "TextSection39", ROMX[$4000], BANK[$a6]
 TextSection39:
   INCBIN cTextSection39
+TextSection39End:
 
 SECTION "TextSection40", ROMX[$4000], BANK[$a7]
 TextSection40:
   INCBIN cTextSection40
+TextSection40End:
 
 SECTION "TextSection41", ROMX[$4000], BANK[$e8]
 TextSection41:
   INCBIN cTextSection41
+TextSection41End:
 
 SECTION "TextSection42", ROMX[$4000], BANK[$a8]
 TextSection42:
   INCBIN cTextSection42
+TextSection42End:
 
 SECTION "TextSection43", ROMX[$4000], BANK[$a9]
 TextSection43:
   INCBIN cTextSection43
+TextSection43End:
 
 SECTION "TextSection44", ROMX[$4000], BANK[$aa]
 TextSection44:
   INCBIN cTextSection44
+TextSection44End:
 
 SECTION "TextSection45", ROMX[$4000], BANK[$ab]
 TextSection45:
   INCBIN cTextSection45
+TextSection45End:
 
 SECTION "TextSection46", ROMX[$4000], BANK[$ac]
 TextSection46:
   INCBIN cTextSection46
+TextSection46End:
 
 SECTION "TextSection47", ROMX[$4000], BANK[$bf]
 TextSection47:
   INCBIN cTextSection47
+TextSection47End:
 
 SECTION "TextSection48", ROMX[$4000], BANK[$e6]
 TextSection48:
   INCBIN cTextSection48
+TextSection48End:
 
 SECTION "TextSection49", ROMX[$4000], BANK[$ad]
 TextSection49:
   INCBIN cTextSection49
+TextSection49End:
 
 SECTION "TextSection50", ROMX[$4000], BANK[$ae]
 TextSection50:
   INCBIN cTextSection50
+TextSection50End:
 
 SECTION "TextSection51", ROMX[$4000], BANK[$af]
 TextSection51:
   INCBIN cTextSection51
+TextSection51End:
 
 SECTION "TextSection52", ROMX[$4000], BANK[$e9]
 TextSection52:
   INCBIN cTextSection52
+TextSection52End:
 
 SECTION "TextSection53", ROMX[$4000], BANK[$ea]
 TextSection53:
   INCBIN cTextSection53
+TextSection53End:
 
 SECTION "TextSection54", ROMX[$4000], BANK[$80]
 TextSection54:
   INCBIN cTextSection54
+TextSection54End:
 
 SECTION "Dialog Text Tables", ROM0[$21ef]
 TextTableBanks:: ; 0x21ef
